44748f5a592d5a804891464c86678b6754f129de
Weliton Martins <weliton.martins@yaman.com>
2020-08-31 22:50:09 -0300

    estrutura inicial

---
+++ src/main/java/br/com/litosolucoes/appsodre/AppSodreApplication.java
@@ -0,0 +1,13 @@
+package br.com.litosolucoes.appsodre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSodreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSodreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/litosolucoes/appsodre/AppSodreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.litosolucoes.appsodre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSodreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
